231153c34c52a30b5b487f3313626c80c1ec8e1e
BernardoBiazzi <bernardo.biazzi@gmail.com>
2022-09-28 22:15:58 -0300

    firstcommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,83 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Jogodavelha } from './jogo-da-velha/jogo-da-velha.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'minimax';
+export class AppComponent implements OnInit {
+
+  public whoStarts = '';
+
+  public jogodavelhaInicial = {
+    blocos: [
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+      undefined,
+    ],
+    points: 0
+  };
+
+  public jogosdavelha: Jogodavelha[] = [];
+  public allPossibilities: Jogodavelha[][] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.select(this.jogodavelhaInicial);
+  }
+
+  select(jogodavelha: Jogodavelha) {
+    if (this.somebodyWins(jogodavelha.blocos)) return alert('Jogo encerrado');
+    if (jogodavelha.points == -1) return alert('Jogo já selecionado anteriormente');
+    if (jogodavelha.points == 0) jogodavelha.points = -1;
+
+    this.whoStarts = this.whoStarts == 'O' ? 'X' : 'O';
+    const Jogodavelha = jogodavelha;
+    let emptySpaces: number[] = [];
+
+    jogodavelha.blocos.forEach((space: any, index: number) => {
+      if (space == undefined) emptySpaces.push(index);
+    });
+
+    let newJogosDaVelha: Jogodavelha[] = [];
+
+    if (emptySpaces) {
+
+      emptySpaces.forEach((emptySpace) => {
+        let blocos = [...Jogodavelha.blocos]
+        blocos[emptySpace] = this.whoStarts;
+        let newJogoDaVelha = {blocos: blocos, points: this.somebodyWins(blocos) ? 1 : 0};
+        newJogosDaVelha.push(newJogoDaVelha);
+      });
+
+      this.allPossibilities.push(newJogosDaVelha);
+    }
+  }
+
+  somebodyWins(blocos: Array<any>): boolean {
+    if (blocos[0] == 'O' && blocos[1] == 'O' && blocos[2] == 'O') return true;
+    if (blocos[0] == 'O' && blocos[4] == 'O' && blocos[8] == 'O') return true;
+    if (blocos[2] == 'O' && blocos[5] == 'O' && blocos[8] == 'O') return true;
+    if (blocos[6] == 'O' && blocos[7] == 'O' && blocos[8] == 'O') return true;
+    if (blocos[0] == 'O' && blocos[3] == 'O' && blocos[6] == 'O') return true;
+    if (blocos[0] == 'X' && blocos[1] == 'X' && blocos[2] == 'X') return true;
+    if (blocos[0] == 'X' && blocos[4] == 'X' && blocos[8] == 'X') return true;
+    if (blocos[2] == 'X' && blocos[5] == 'X' && blocos[8] == 'X') return true;
+    if (blocos[6] == 'X' && blocos[7] == 'X' && blocos[8] == 'X') return true;
+    if (blocos[0] == 'X' && blocos[3] == 'X' && blocos[6] == 'X') return true;
+    else return false;
+  }
+
+  calculaMelhorOpcao() {
+
+  }
+
 }
---
+++ src/app/jogo-da-velha/jogo-da-velha.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+export interface Jogodavelha {
+  blocos: Array<any>,
+  points: number
+}
+
+@Component({
+  selector: 'app-jogo-da-velha',
+  templateUrl: './jogo-da-velha.component.html',
+  styleUrls: ['./jogo-da-velha.component.scss']
+})
+export class JogoDaVelhaComponent implements OnInit {
+
+  @Input() jogodavelha: Jogodavelha = {blocos: [], points: 0};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
